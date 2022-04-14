bdf9f5806e4c7fb84ef2af20c78ac29a1b68677e
sikvelsigma <63592183+sikvelsigma@users.noreply.github.com>
2022-04-14 21:41:41 +0300

    Update LandingContract.sol

--- contracts/LandingContract.sol
+++ contracts/LandingContract.sol
@@ -5,47 +5,63 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface LandingToken {
     function mint(address account, uint256 amount) external;
+
     function burn(address account, uint256 amount) external;
 }
 
 contract LandingContracts is Ownable {
-
-    uint public immutable minimumDuration;
-    uint public immutable maximumDuration;
+    uint256 public immutable minDuration;
+    uint256 public immutable maxDuration;
+    uint256 public immutable maxFee;
+    uint256 public immutable minFee;
     LandingToken public immutable token;
 
     struct Customer {
-        uint amount;
-        uint duration;
-    } 
+        uint256 amount;
+        uint256 duration;
+        bool active;
+    }
 
-    mapping (address => Customer) customers;
+    mapping(address => Customer) customers;
 
+    modifier notActive() {
+        require(customers[msg.sender].active == false);
+        _;
+    }
+    modifier onlyActive() {
+        require(customers[msg.sender].active == true);
+        _;
+    }
 
-    constructor(uint _minimumDuration, uint _maximumDuration, LandingToken _token) {
-        minimumDuration = _minimumDuration;
-        maximumDuration = _maximumDuration;
+    constructor(
+        uint256 _minDuration,
+        uint256 _maxDuration,
+        uint256 _maxFee,
+        uint256 _minFee,
+        LandingToken _token
+    ) {
+        minDuration = _minDuration;
+        maxDuration = _maxDuration;
+        maxFee = _maxFee;
+        minFee = _minFee;
         token = _token;
     }
 
-    function borrowTokens(uint _amount, uint _duration) external payable {
-
+    function borrowTokens(uint256 _amount, uint256 _duration)
+        external
+        payable
+        notActive
+    {
+        customers[msg.sender].amount = _amount;
+        customers[msg.sender].duration = _duration;
+        customers[msg.sender].active = true;
     }
 
-    function returnTokens() external {
+    function returnTokens() external onlyActive {}
 
-    }
+    function withdrawEth() external onlyActive {}
 
-    function withdrawEth() external {
+    function withdrawFeeContractEth() external onlyOwner {}
 
-    }
-
-    function withdrawFeeContractEth() external onlyOwner {
-
-    }
-
-    function withdrawOverdraftContractEth() external onlyOwner {
-
-    }
-
+    function withdrawOverdraftContractEth() external onlyOwner {}
 }
